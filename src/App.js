309a34aae424e540c1880bb180b105158bb01ec5
Akash ks <akashks8855@gmail.com>
2022-10-08 13:14:58 +0530

    card shuffle function works

--- src/App.js
+++ src/App.js
@@ -1,73 +1,56 @@
 import './App.css';
-import { Card1,Card2,Card3 } from './components/Content';
-import React, {useState,useEffect} from 'react';
+import { Card1,Card2,Card3,Card4 } from './components/Content';
+import React, {useState} from 'react';
 
 function App() {
 
   const [text1,settext1] = useState(<Card1/>);
     const [text2,settext2] = useState(<Card2/>);
     const [text3,settext3] = useState(<Card3/>);
-    
-
-    useEffect(() => {
-        const change=()=>{
+    const [text4,settext4] = useState(<Card4/>);
+    const [score,setscore] = useState(0);
+   
+    const change=()=>{
             var arr = [];
-            while(arr.length < 3){
-                var r = Math.floor(Math.random() * 3) + 1;
+            while(arr.length < 4){
+                var r = Math.floor(Math.random() * 4) + 1;
                 if(arr.indexOf(r) === -1) {
                   arr.push(r);
                 }
             }
-            console.log(arr);
 
-        if(arr[0]===1){
-           settext1(<Card1/>);
+       const check=(num,set)=>{
+        if(arr[num]===1){
+          set(<Card1/>);
+         }
+        else if(arr[num]===2){
+         set(<Card2/>);
         }
-        else if(arr[0]===2){
-          settext1(<Card2/>);
-        }
+        else if(arr[num]===3){
+          set(<Card3/>);
+         }
         else{
-          settext1(<Card3/>);
+          set(<Card4/>);
         }
-        
-        if(arr[1]===1){
-          settext2(<Card1/>);
        }
-       else if(arr[1]===2){
-         settext2(<Card2/>);
-       }
-       else{
-         settext2(<Card3/>);
-       } 
 
-       if(arr[2]===1){
-        settext3(<Card1/>);
-       }
-     else if(arr[2]===2){
-       settext3(<Card2/>);
-     }
-     else{
-       settext3(<Card3/>);
-     } 
+        check(0,settext1);
+        check(1,settext2);
+        check(2,settext3); 
+        check(3,settext4);
 
-        };
+    };
 
-        document.addEventListener("click",change);
-        return () => {
-          document.removeEventListener("click", change);
-        };
 
-    
-
-    },[text1,text2,text3])
 
 
 
   return (
     <div className="App">
-      <div>{text1}</div>
-      <div>{text2}</div>
-      <div>{text3}</div>
+      <div onClick={change} >{text1}</div>
+      <div onClick={change} >{text2}</div>
+      <div onClick={change} >{text3}</div>
+      <div onClick={change} >{text4}</div>
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,30 +1,41 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 
+const Card1=(props)=>{
 
+ 
 
-const Card1=()=>{
     return(
-      <div style={{color: "white",backgroundColor: "red",}}>
-          card 1
+      <div  style={{color: "black"}}>
+          <img src={require('../images/p1.jpeg')} alt="p1" />
       </div>
     );
 }
 
 const Card2=()=>{
     return(
-      <div style={{color: "white",backgroundColor: "black",}}>
-           card 2
+      <div style={{color: "black"}}>
+          <img src={require('../images/p2.jpeg')} alt="p2" />
       </div>
     );
 }
 
 const Card3=()=>{
     return(
-      <div style={{color: "white",backgroundColor: "grey",}}>
-           card 3
+      <div style={{color: "black"}}>
+         <img src={require('../images/p3.jpeg')} alt="p3" />
       </div>
     );
 }
 
-export {Card1,Card2,Card3};
+const Card4=()=>{
+  return(
+    <div style={{color: "black"}}>
+       <img src={require('../images/p4.jpeg')} alt="p4" />
+    </div>
+  );
+}
+
+
+
+export {Card1,Card2,Card3,Card4};
