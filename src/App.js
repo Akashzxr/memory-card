bacd3badb1521f42b6c1faa9e67682c3a28f966e
Akash ks <akashks8855@gmail.com>
2022-10-09 12:19:23 +0530

    shuffling using grid sucess

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
 import './App.css';
-import { Card1,Card2,Card3,Card4 } from './components/Content';
-import React, {useState} from 'react';
+import { Card1,Card2,Card3,Card4,Score } from './components/Content';
+import React, {useEffect, useState} from 'react';
 
 function App() {
 
-  const [text1,settext1] = useState(<Card1/>);
+ 
+ 
+    const sendDataToParent = () => { 
+      const s=true;
+      if(s){
+        setc1(c1+1);
+        console.log(c1);
+      }
+      
+     
+    };
+    
+
+  
+
+  
+  const [score,setscore] = useState(0);
+  const [text1,settext1] = useState(<Card1 sendDataToParent={sendDataToParent}/>);
     const [text2,settext2] = useState(<Card2/>);
     const [text3,settext3] = useState(<Card3/>);
     const [text4,settext4] = useState(<Card4/>);
-    const [score,setscore] = useState(0);
+    
+  
+    
    
     const change=()=>{
             var arr = [];
-            while(arr.length < 4){
-                var r = Math.floor(Math.random() * 4) + 1;
+            while(arr.length < 2){
+                var r = Math.floor(Math.random() * 2) + 1;
                 if(arr.indexOf(r) === -1) {
                   arr.push(r);
                 }
             }
 
-       const check=(num,set)=>{
+            var a = [];
+            while(a.length < 2){
+                var r = Math.floor(Math.random() * 2) + 1;
+                if(a.indexOf(r) === -1) {
+                  a.push(r);
+                }
+            }
+
+            
+            
+      /* const check=(num,set)=>{
         if(arr[num]===1){
-          set(<Card1/>);
+          set(<Card1 sendDataToParent={sendDataToParent}/>);
          }
         else if(arr[num]===2){
          set(<Card2/>);
@@ -37,20 +66,44 @@ function App() {
         check(0,settext1);
         check(1,settext2);
         check(2,settext3); 
-        check(3,settext4);
+        check(3,settext4);*/
+
+        setscore(score+1);
+        setcolumn1(arr[0])
+        setcolumn2(arr[1]);
+        setcol1(arr[0]+1);
+        setcol2(arr[1]+1);
+
+        setrow1(a[0]);
+        setrow2(a[1]);
+        setro1(a[0]+1);
+        setro2(a[1]+1);
 
     };
 
 
+    const [c1,setc1] = useState(0);
+    const [column1,setcolumn1] = useState(1);
+    const [column2,setcolumn2] = useState(2);
+    const [col1,setcol1] = useState(2);
+    const [col2,setcol2] = useState(3);
+
+    const [row1,setrow1] =useState(1);
+    const [row2,setrow2] =useState(2);
+    const [ro1,setro1] =useState(2);
+    const [ro2,setro2] =useState(3);
 
 
-
+    
   return (
+    <div>
+      <Score s={score}/>
     <div className="App">
-      <div onClick={change} >{text1}</div>
-      <div onClick={change} >{text2}</div>
-      <div onClick={change} >{text3}</div>
-      <div onClick={change} >{text4}</div>
+      <div onClick={change} style={ { gridColumn: `${column1}/${col1}`,gridRow: `${row1}/${ro1}`} } ><Card1/></div>
+      <div onClick={change} style={ { gridColumn: `${column2}/${col2}`,gridRow: `${row1}/${ro2}`} }><Card2/></div>
+      <div onClick={change} style={ { gridColumn: `${column1}/${col1}`,gridRow: `${row2}/${ro2}`} }><Card3/></div>
+      <div onClick={change} style={ { gridColumn: `${column2}/${col2}`,gridRow: `${row2}/${ro2}`} }><Card4/></div>
+    </div>
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
 
 
-const Card1=(props)=>{
 
- 
 
+
+const Card1=({sendDataToParent})=>{
+
+  const [count,setcount] = useState(0);
+   const change=()=>{
+    setcount(count+1);
+    console.log(count);
+   }
+  
     return(
-      <div  style={{color: "black"}}>
+      <div id="n" onClick={change} style={{color: "black"}}>
+        1
           <img src={require('../images/p1.jpeg')} alt="p1" />
       </div>
     );
@@ -14,7 +22,7 @@ const Card1=(props)=>{
 
 const Card2=()=>{
     return(
-      <div style={{color: "black"}}>
+      <div style={{color: "black"}}>2
           <img src={require('../images/p2.jpeg')} alt="p2" />
       </div>
     );
@@ -22,7 +30,7 @@ const Card2=()=>{
 
 const Card3=()=>{
     return(
-      <div style={{color: "black"}}>
+      <div style={{color: "black"}}>3
          <img src={require('../images/p3.jpeg')} alt="p3" />
       </div>
     );
@@ -30,12 +38,18 @@ const Card3=()=>{
 
 const Card4=()=>{
   return(
-    <div style={{color: "black"}}>
+    <div style={{color: "black"}}>4
        <img src={require('../images/p4.jpeg')} alt="p4" />
     </div>
   );
 }
 
+const Score=({s})=>{
+  return(
+    <div>
+      <h1>score={s}</h1>
+    </div>
+  )
+}
 
-
-export {Card1,Card2,Card3,Card4};
+export {Card1,Card2,Card3,Card4,Score};
